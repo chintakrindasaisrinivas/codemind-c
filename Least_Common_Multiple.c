54e98c6aa241cf7e9ad2ded68215c2ac9df33bb8
chintakrindasaisrinivas <119840621+chintakrindasaisrinivas@users.noreply.github.com>
2022-12-06 11:17:06 +0530

    #include<stdio.h> int main() {     long i,m,n,c,hcf=1;     scanf("%ld%ld",&m,&n);     if(m>n)     c=n;     else     c=m;     for(i=1;i<=c;i++)     {         if(n%i==0 && m%i==0)         hcf=i;     }     printf("%d",(m*n)/hcf); }

---
+++ Least_Common_Multiple.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    long i,m,n,c,hcf=1;
+    scanf("%ld%ld",&m,&n);
+    if(m>n)
+    c=n;
+    else
+    c=m;
+    for(i=1;i<=c;i++)
+    {
+        if(n%i==0 && m%i==0)
+        hcf=i;
+    }
+    printf("%d",(m*n)/hcf);
+}
