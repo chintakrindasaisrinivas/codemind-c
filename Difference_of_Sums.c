08e2023bdff21c3dc1dd3eb4d15b8f9604805e7a
chintakrindasaisrinivas <119840621+chintakrindasaisrinivas@users.noreply.github.com>
2022-12-19 19:39:07 +0530

    #include<stdio.h> int main() {     int i,n,s=0,j=0,c;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         s=s+i;         j=j+i*i;     }     c=s*s-j;     printf("%d",c); }

---
+++ Difference_of_Sums.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,s=0,j=0,c;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        s=s+i;
+        j=j+i*i;
+    }
+    c=s*s-j;
+    printf("%d",c);
+}
