9477a6eed5c995bedc8321f997ec8b339b1272df
chintakrindasaisrinivas <119840621+chintakrindasaisrinivas@users.noreply.github.com>
2022-12-27 12:13:49 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=i;j++)         {             if(j==1 || j==i || i==n)             {                 printf("*");             }             else             {                 printf(" ");             }         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,13 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,m;
-    scanf("%d",&m);
-    for(i=m;i>=1;i--)
+    int i,j,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
     {
         for(j=1;j<=i;j++)
         {
-            printf("%d",j);
+            if(j==1 || j==i || i==n)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
         }
         printf("
 ");
